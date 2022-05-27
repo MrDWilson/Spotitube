6dfe5f42f99791312feedeb7ba9a2b15d428224c
MrDWilson <d.wilson2306@gmail.com>
2022-05-27 11:16:21 +0100

    Added in missing service

--- manager/Program.cs
+++ manager/Program.cs
@@ -1,3 +1,5 @@
+using Spotitube.Services.Url;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<IUrlHandler, UrlHandler>();
 
 var app = builder.Build();
 
