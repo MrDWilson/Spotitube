e436cd145df2f437c702465b9c73330aa315b654
MrDWilson <d.wilson2306@gmail.com>
2022-05-27 13:57:16 +0100

    Added playlist code

--- manager/Services/Playlist/PlaylistService.cs
+++ manager/Services/Playlist/PlaylistService.cs
@@ -4,6 +4,17 @@ public class PlaylistService : IPlaylistService
 {
     async public Task<string> Convert(IEnumerable<string> youtubeLinks) 
     {
-        return "";
+        string playlistName = Guid.NewGuid().ToString() + ".txt";
+        string? playlistFolder = Environment.GetEnvironmentVariable("PLAYLIST_FOLDER");
+        if(playlistFolder == null)
+        {
+            throw new Exception("PLAYLIST_FOLDER env variable missing");
+        }
+
+        string allLinks = string.Join(Environment.NewLine, youtubeLinks);
+        string newPlaylistFile = Path.Join(playlistFolder, playlistName);
+        await File.WriteAllTextAsync(newPlaylistFile, allLinks);
+
+        return playlistName;
     }
 }
