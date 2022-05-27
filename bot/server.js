e5061ccce3feb04469c6fc750f2ce5081a7fe538
Danny Wilson <d.wilson2306@gmail.com>
2022-05-27 16:20:07 +0100

    Final fixes

--- bot/server.js
+++ bot/server.js
@@ -1,6 +1,6 @@
 const { Client, Intents } = require('discord.js');
 const axios = require('axios');
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
 const makeRequest = async (url) => await (await axios.get(url));
 const makePostRequest = async (url, data) => await (await axios.post(url, data));
@@ -9,7 +9,9 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on("message", (message) => {
+client.on("messageCreate", (message) => {
+    console.log(message);
+    console.log(message.content);
     if(message.content.startsWith("~") || message.content.startsWith("#")) {
         let command = message.content.slice(1);
 
@@ -19,7 +21,7 @@ client.on("message", (message) => {
                 url: url
             };
             makePostRequest("http://spotitube_manager", data).then(res => {
-                message.channel.send("~play " + res);
+                message.channel.send("~play " + res.data);
             });
         }
         else if(command.startsWith("clearcache")) {
