26a9f350744baa2d23e74c2c40c759a68d59dd8a
MrDWilson <d.wilson2306@gmail.com>
2022-05-27 19:43:36 +0100

    Token logging

--- token/app.py
+++ token/app.py
@@ -31,6 +31,8 @@ expiry: Optional[DateTime] = None
 def get_token():
     global token
     global expiry
+    print('Token: ' + token)
+    print('Expires on: ' + expiry.strftime("%m/%d/%Y, %H:%M:%S"))
     if token is not None and expiry is not None and expiry < datetime.now():
         return token
 
